93b2b9571b550793054cd2a72fb37582eacd7614
Kaosheep <littlekaotw@gmail.com>
2023-10-28 15:17:36 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted, ref, reactive } from "vue";
+
+const count = ref(0);
+let arr = reactive({
+  data:{}
+});
+const obj = reactive({
+  name: "sheep",
+  age: 28
+})
+const incre = () => {
+  count.value++;
+}
+onMounted(() => {
+
+})
+</script>
+
+<template>
+  
+</template>
+
+<style scoped></style>
